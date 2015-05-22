03be1b0c962bbf11f4302776e678e307f917f575
yashodhandivakaran <yashodhandivakaran@gmail.com>
2015-05-23 01:55:37 +0530

    -Twitter digit added
    Signed-off-by: yashodhandivakaran <yashodhandivakaran@gmail.com>

--- InMobiHackApp/app/src/main/java/hack/inmobi/com/inmobihackapp/MainActivity.java
+++ InMobiHackApp/app/src/main/java/hack/inmobi/com/inmobihackapp/MainActivity.java
@@ -4,13 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "w3OPBdB0akwml9S5wCHZ21qcl";
+    private static final String TWITTER_SECRET = "zW1zforpAkl53F359czwgK27rg3pvk9oS7FmTObOCP8Q7xaEUM";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits());
         setContentView(R.layout.activity_main);
     }
 
